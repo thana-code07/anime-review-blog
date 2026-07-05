ad41c85bc5f786d719fcf70185a303f29bb29b6d
Thanathat Lumsan <thanathatlumsan@gmail.com>
2026-07-05 16:32:18 +0700

    Merge branch 'feat/not-found-page'

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 
 import { BlogPostPage } from "./pages/BlogPostPage";
 import { LandingPage } from "./pages/LandingPage";
+import { NotFoundPage } from "./pages/NotFoundPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/post/:postId" element={<BlogPostPage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 }
